334b7a7f1311dd2f6c71d8e8b27d99f76ffef32a
ZahidRamirez00 <zahidramirez.00@gmail.com>
2023-01-17 15:12:54 +0100

    Aggiungere i file di progetto.

---
+++ La-Mia-Pizzeria-Crud-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace La_Mia_Pizzeria_Crud_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
