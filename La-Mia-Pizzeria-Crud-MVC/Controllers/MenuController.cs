72a252a2f96a16d365a8ba0752267d5e4791796e
ZahidRamirez00 <zahidramirez.00@gmail.com>
2023-01-17 15:42:32 +0100

    Recupero file dal progetto sul menù della pizzeria

---
+++ La-Mia-Pizzeria-Crud-MVC/Controllers/MenuController.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using La_Mia_Pizzeria_1.Models;
+using La_Mia_Pizzeria_1.Utils;
+using System.Diagnostics;
+using La_Mia_Pizzeria_1.DataBase;
+using Microsoft.Extensions.Hosting;
+
+namespace La_Mia_Pizzeria_1.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            using(PizzeContext db = new PizzeContext())
+            {
+                //List<Pizza> listaDellePizze = PizzeData.GetPizze();
+                List<Pizza> listaDellePizze = db.Pizze.ToList<Pizza>();
+                return View("Index", listaDellePizze);
+            }
+        }
+
+        public IActionResult Details(int id)
+        {
+            using(PizzeContext db = new PizzeContext())
+            {
+                Pizza pizzaTrovato = db.Pizze
+                    .Where(SingolaPizzaNelDb => SingolaPizzaNelDb.Id == id)
+                    .FirstOrDefault();
+
+                if (pizzaTrovato != null)
+                {
+                    return View(pizzaTrovato);
+                }
+                
+                return NotFound("La pizza con l'id cercato non esiste!");
+            }
+            /*List<Pizza> listaDellePizze = PizzeData.GetPizze();
+
+            foreach (Pizza pizza in listaDellePizze)
+            {
+                if (pizza.Id == id)
+                {
+                    return View(pizza);
+                }
+            }
+
+            return NotFound("Il post con l'id cercato non esiste!");*/
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza formData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", formData);
+            }
+
+            using (PizzeContext db = new PizzeContext())
+            {
+                db.Pizze.Add(formData);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            using (PizzeContext db = new PizzeContext())
+            {
+                Pizza pizzaToUpdate = db.Pizze.Where(Pizza => Pizza.Id == id).FirstOrDefault();
+
+                if (pizzaToUpdate == null)
+                {
+                    return NotFound("La pizza non è stata trovata");
+                }
+
+                return View("Update", pizzaToUpdate);
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Pizza formData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Update", formData);
+            }
+
+            using (PizzeContext db = new PizzeContext())
+            {
+                Pizza pizzaToUpdate = db.Pizze.Where(Pizza => Pizza.Id == formData.Id).FirstOrDefault();
+
+                if (pizzaToUpdate != null)
+                {
+                    pizzaToUpdate.Name = formData.Name;
+                    pizzaToUpdate.Description = formData.Description;
+                    pizzaToUpdate.Image = formData.Image;
+                    pizzaToUpdate.Prezzo = formData.Prezzo;
+
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return NotFound("La pizza che volevi modificare non è stata trovata!");
+                }
+            }
+
+        }
+
+        
+        public IActionResult Delete(int id)
+        {
+            using (PizzeContext db = new PizzeContext())
+            {
+                Pizza pizzaToDelete = db.Pizze.Where(Pizza => Pizza.Id == id).FirstOrDefault();
+
+                if (pizzaToDelete != null)
+                {
+                    db.Pizze.Remove(pizzaToDelete);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return NotFound("La pizza da eliminare non è stata trovata!");
+                }
+            }
+        }
+    }
+}
---
+++ La-Mia-Pizzeria-Crud-MVC/Models/Pizza.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace La_Mia_Pizzeria_1.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        [StringLength(100, ErrorMessage = "Il campo titolo non può contenere più di 100 caratteri")]
+        public string Name { get; set; }
+        [Required]
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+        [Required]
+        [Url]
+        [StringLength(300, ErrorMessage = "Il campo titolo non può contenere più di 300 caratteri")]
+        public string Image { get; set; }
+        
+        [Required]
+        [Range(1, 100)]
+        public double Prezzo { get; set; }
+
+        public Pizza() { }
+        public Pizza(string name, string description, string image, double prezzo)
+        {
+            Name = name;
+            Description = description;
+            Image = image;
+            Prezzo = prezzo;    
+        }
+    }
+}
